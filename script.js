c22f7e7a4eb3458f9e16c527b73c1842b0cd2132
Shubham Chaudhari <sonuc296@gmail.com>
2024-09-20 01:18:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+let contentDisplay = "";
+document.querySelector("#Display").value = contentDisplay;
+
+let everyoneLovesShubham = true;
+console.log(everyoneLovesShubham);
+
+typeof everyoneLovesShubham;
+console.log(typeof everyoneLovesShubham);
